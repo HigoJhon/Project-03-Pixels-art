52f6f4652f8fe4f8ecf8379f2ffab1de99c4c92f
Higo Jhon <higojhon98@gmail.com>
2022-08-31 20:17:01 -0300

    alterando cor da paleta 1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function cores() {
     let div1 = document.getElementsByTagName("div")[1];
-    div1.style.backgroundColor = "blue";
+    div1.style.backgroundColor = "black";
     let div2 = document.getElementsByTagName("div")[2];
     div2.style.backgroundColor = "red";
     let div3 = document.getElementsByTagName("div")[3];
     div3.style.backgroundColor = "yellow";
     let div4 = document.getElementsByTagName("div")[4];
     div4.style.backgroundColor = "aqua";
-}cortes()
+}cores()
 
 
