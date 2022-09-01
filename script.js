2725483b20e9b42439612b84dcee7f1778527ffb
Higo Jhon <higojhon98@gmail.com>
2022-09-01 20:16:45 -0300

    Add quadro 5x5 e altura e largura 40px

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-//aprendi assistindo em um canal no youtube chamado zigzag.
+//aprendi assistindo em um canal no youtube chamado zigzag
 function mudarCores() {
-    let codigo ="123456789ABCDEF"
+    let codigo = "123456789ABCDEF"
     let jogodavelha = "#"
-    let resultado; 
+    let resultado;
 
-    for (let index = 0;index < 6 ;index += 1){
+    for (let index = 0; index < 6; index += 1) {
         resultado = jogodavelha += codigo.charAt(Math.floor(Math.random() * codigo.length));
     }
-   return resultado
-} 
-function clicou(){
+    return resultado
+}
+function clicou() {
     let resultado;
-    for (let index = 1; index < 4 ;index += 1){
-        let cores = document.getElementById(`cor${index+1}`);
+    for (let index = 1; index < 4; index += 1) {
+        let cores = document.getElementById(`cor${index + 1}`);
         resultado = cores.style.background = mudarCores();
+        
+        
     } return resultado;
 }
-
+function salvarCor() {
+    let resultado = document.getElementsByClassName("color")
+    localStorage.setItem("colorPalette", resultado);
+    
+} salvarCor()
 
