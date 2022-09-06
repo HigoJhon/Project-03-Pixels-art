0af6d3678c48db459bbf106d708d02f228d4213b
Higo Jhon <higojhon98@gmail.com>
2022-09-06 16:27:18 -0300

    selected ao click

--- script.js
+++ script.js
@@ -18,18 +18,33 @@ function clicou() {
         resultado.push(cores.style.backgroundColor);
 
     } localStorage.setItem("colorPalette", JSON.stringify(resultado))
-} 
+}
 
 function coresSalvas() {
-    if (localStorage.getItem("colorPalette") === null){
-        return clicou()
+    if (localStorage.getItem("colorPalette") === null) {
+        return clicou();
     }
 
 
     const corzinha = JSON.parse(localStorage.getItem("colorPalette"));
     for (let index = 1; index < 4; index += 1) {
         let cores = document.getElementsByClassName("color");
-        cores[index].style.backgroundColor = corzinha[index-1];
+        cores[index].style.backgroundColor = corzinha[index - 1];
     }
-}coresSalvas()
+} coresSalvas();
+
+const todasCores = document.getElementsByClassName("color");
+  for (let index = 0 ; index < todasCores.length; index += 1){
+    todasCores[index].addEventListener("click", select);
+  }
+    
+
+function select(func) {
+    let input = func.target;
+
+    const selec = document.querySelector(".selected");
+    selec.classList.remove("selected");
+    input.classList.add("selected");    
+}
+
 
