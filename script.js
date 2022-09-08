a26168c741ba81dc1723b57534634935e0c818af
Higo Jhon <higojhon98@gmail.com>
2022-09-07 21:02:16 -0300

    add localStorage para os quadrados

--- script.js
+++ script.js
@@ -24,8 +24,6 @@ function coresSalvas() {
     if (localStorage.getItem("colorPalette") === null) {
         return clicou();
     }
-
-
     const corzinha = JSON.parse(localStorage.getItem("colorPalette"));
     for (let index = 1; index < 4; index += 1) {
         let cores = document.getElementsByClassName("color");
@@ -40,11 +38,10 @@ for (let index = 0; index < todasCores.length; index += 1) {
 
 
 function select(func) {
-    let input = func.target;
-
-    const selec = document.querySelector(".selected");
-    selec.classList.remove("selected");
-    input.classList.add("selected");
+  let input = func.target;
+  const selec = document.querySelector(".selected");
+  selec.classList.remove("selected");
+  input.classList.add("selected");
 }
 
 const corSelec = document.getElementsByClassName("selected");
@@ -56,10 +53,30 @@ for (let index = 0; index < div.length; index += 1) {
 function colorirDiv(pix) {
   let input = pix.target;
   input.style.backgroundColor = corSelec[0].style.backgroundColor;
+ quadradoSalvo()
 }
 function limpar() {
   const divs = document.querySelectorAll('.pixel');
   for (let index = 0; index < divs.length; index += 1) {
     divs[index].style.background = 'white';
   }
+quadradoSalvo();
 }
+function quadradoSalvo() {
+  const resultado = [];
+  for (let index = 0; index < div.length; index += 1) {
+    let cores = div[index]
+    resultado.push(cores.style.backgroundColor);
+  }
+  localStorage.setItem("pixelBoard", JSON.stringify(resultado))
+}
+function SalvCorDiv() {
+    if (localStorage.getItem("pixelBoard") === null) {
+        return quadradoSalvo();
+    }
+    const corzinha = JSON.parse(localStorage.getItem("pixelBoard"));
+    for (let index = 0; index < div.length; index += 1) {
+        div[index].style.backgroundColor = corzinha[index];
+    }
+}
+SalvCorDiv();
